a5b39592856a9f6282a78bbef108ea373ee8e7b7
JQ <2016quezadaj@gmail.com>
2020-09-18 20:57:10 -0700

    Added database init functions

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -24,8 +24,16 @@ def create_app(test_config=None):
         pass
 
     # a simple page that says hello
-    @app.route('/hello')
-    def hello():
-        return 'Hello, World!'
+    # @app.route('/hello')
+    # def hello():
+    #     return 'Hello, World!'
 
-    return app
+    # return app
+    def create_app():
+        app = ...
+        # existing code omitted
+
+        from . import db
+        db.init_app(app)
+
+        return app
